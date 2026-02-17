c3c18e11e30b9e0d3a366b4c6051861e3e9d0089
kiran <kamsalisai19@gmail.com>
2026-02-17 08:46:51 +0530

    04-variables.sh

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=SURESH #no space between value and =
+PERSON2=RAMESH
+
+echo "$PERSON1:: hello ramesh, how are you?"
+echo "ramesh:: hi $PERSON1, i am good. how about you?"
+echo "$PERSON1:: i am doing well, thanks for asking. what are you learning $PERSON2?"
+echo "$PERSON2:: i am learning shell scripting. it is really interesting!"
