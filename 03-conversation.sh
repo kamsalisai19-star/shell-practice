0c3c61931ceaa91e9a5bcdad67bab6d2779fdc4c
kiran <kamsalisai19@gmail.com>
2026-02-17 08:32:36 +0530

    add 03 conversation script

---
+++ 03-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "suresh:: hello ramesh, how are you?"
+echo "ramesh:: hi suresh, i am good. how about you?"
+echo "suresh:: i am doing well, thanks for asking. what are you learning ramesh?"
+echo "ramesh:: i am learning shell scripting. it is really interesting!"
+
