8cb5ef2ebbe758795f272d2d11184a8b0b276165
kiran <kamsalisai19@gmail.com>
2026-02-28 13:17:45 +0530

    actions

--- 20-trap.sh
+++ 20-trap.sh
@@ -1,14 +1,13 @@
-
 #!/bin/bash
 
 set -e
+trap 'echo "There is an error in $LINENO, Command: $BASH_COMMAND"' ERR
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
-
 R="\e[31m"
-G="\e[31m"
+G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
@@ -19,11 +18,11 @@ fi
 
 mkdir -p $LOGS_FOLDER
 
-for package in $@   # sudo sh 14-loops.sh nginx mysql nodejs
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
 do
     dnf list installed $package &>>$LOGS_FILE
     if [ $? -ne 0 ]; then
-        echo "$package not installed, installing now"
+        echo -e "$package not installed, installing now"
         dnf install $package -y &>>$LOGS_FILE
     else
         echo -e "$package already installed ... $Y SKIPPING $N"
